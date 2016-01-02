0bea70eaf7fae0b7bca7eadda6e9c665d9827f61
spam4kev <spam4kev@gmail.com>
2016-01-02 13:01:08 -0500

    might be ready now

--- razor-entrypoint.sh
+++ razor-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i 's/razor_prd/\/\/razor_db\/razor_prd/' /etc/razor/config.yaml
 curl -SL http://links.puppetlabs.com/razor-microkernel-latest.tar \
-     | tar -xJC /var/lib/razor/repo-store
+     | tar -xC /var/lib/razor/repo-store/
 razor-admin -e production migrate-database
 /etc/razor/razor-server.sh start
