71cdfdd2832bfa092c8c585773a7e6ca5e0061da
spam4kev <spam4kev@gmail.com>
2016-01-02 15:01:28 -0500

    wip

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker run -d --name razor_db -e POSTGRESQL_USER=razor -e POSTGRESQL_PASSWORD=$1 -e POSTGRESQL_DATABASE=razor_prd -p 5432:5432 -v /data/pgsql:/var/lib/pgsql/data centos/postgresql-94-centos7
-# Troubleshooting steps
-#sudo docker exec -i -t razor_db bash
-#docker logs razor_db
-#psql -h localhost -p 5432 -d razor -U razor
